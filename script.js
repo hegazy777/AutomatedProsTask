5ad34d641835bf74311da58a96745e10fad2c849
hegazy777 <70011414+hegazy777@users.noreply.github.com>
2024-02-28 09:14:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.from(".about", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    delay: 0.5
+  });
+  
+gsap.from(".navbar", {
+    duration: 1,
+    y: -100,
+    opacity: 0,
+    ease: "power2.out"
+  });
+  
+
